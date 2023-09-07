0fc935f195fc65d4e473a989bef35d1271522685
Katalina Torres Carrion <113036170+torrsk@users.noreply.github.com>
2023-09-07 08:27:47 -0500

    Add files via upload

---
+++ algoritmo1.js
@@ -0,0 +1,6 @@
+let sueldo = prompt("ingrese su sueldo: ")
+let nombre = prompt("ingrese su nombre: ")
+
+
+for(let i =0; i<)
+document.write("los empleados que ganan mas del sueldo promedio son: ")
